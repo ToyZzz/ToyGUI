620df6d1c9817b165407d1af2805aa6f8e040ffb
ToyZzz <584638298@qq.com>
2018-11-02 18:09:18 +0800

    new plan

---
+++ ToyGUI/Assets/FairyGUIPlus/UIPackageItem/UIPackageItemAtlas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FairyGUIPlus
+{
+    public class UIPackageItemAtlas : UIPackageItemBase
+    {
+        public int m_width;
+
+        public int m_height;
+
+        public string m_file;
+
+        public NTexture m_ntexture;
+
+
+    }
+}
---
+++ ToyGUI/Assets/FairyGUIPlus/UIPackageItem/UIPackageItemBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FairyGUIPlus
+{
+    public enum UIPackageItemType
+    {
+        Image,
+        MovieClip,
+        Sound,
+        Component,
+        Atlas,
+        Font,
+        Swf,
+        Misc,
+        Unknown
+    }
+
+    public class UIPackageItemBase
+    {
+        public UIPackage m_owner;
+
+        public UIPackageItemType m_type;
+
+        public string m_id;
+
+        public virtual object Load()
+        {
+            return null;
+        }
+    }
+}
+
